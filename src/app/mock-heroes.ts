51e2d83d8fea50f5d70b9654e8fac7f368cbf256
Simon Rabuogi <symokevo@gmail.com>
2023-11-07 15:50:51 +0300

    Display The Heroes' Selection List
    - Enable user selection of a hero and see that hero's details
    - Use *ngFor to Display the List
    - Use *ngIf to conditionally include or exclude particular html block
    - Enable toggling css style class with class binding

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Eskei'},
+    { id: 12, name: 'Kevin'},
+    { id: 13, name: 'Rabbs'},
+    { id: 14, name: 'Aceteasea'},
+    { id: 15, name: 'Zimastack'},
+    { id: 16, name: 'Terasofts'},
+    { id: 17, name: 'SuuefTech'},
+    { id: 18, name: 'Shadow'},
+    { id: 19, name: 'Daudi'},
+    { id: 20, name: 'Abdul'},
+    { id: 21, name: 'Hakim'},
+    { id: 22, name: 'Swalleh'}
+];
